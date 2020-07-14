3df0532077609c53fa1d2fe55e6dfd739b654bf4
Jonatan <tuskerfieldmac@Jony-MacBook-Pro.local>
2020-07-13 20:58:40 -0500

    actions and reducer

---
+++ src/actions/postsActions.js
@@ -0,0 +1,16 @@
+export const GET_POSTS = 'GET_POSTS'
+export const GET_POSTS_SUCCESS = 'GET_POSTS_SUCCESS'
+export const GET_POSTS_FAILURE = 'GET_POSTS_FAILURE'
+
+export const getPosts = () => ({
+    type: GET_POSTS,
+})
+
+export const getPostsSuccess = (posts) => ({
+    type: GET_POSTS_SUCCESS,
+    payload: posts,
+})
+
+export const getPostFailure = () => ({
+    type: GET_POSTS_FAILURE,
+})
